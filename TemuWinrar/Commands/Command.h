f449effe662ba0ba783599bb0a32469fa1fcac3a
Boyan Sinilkov <boiansinilkov@abv.bg>
2026-02-10 16:46:03 +0200

    Command Interface added

--- TemuWinrar/Commands/Command.h
+++ TemuWinrar/Commands/Command.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <string>
+#include <vector>
+
+class Command {
+public:
+    explicit Command(const std::string& name) : callName(name) {}
+    virtual ~Command() = default;
+
+    virtual const std::string& name() const noexcept { return callName; }
+
+    virtual bool execute(const std::vector<std::string>& args) = 0;
+    virtual std::string description() const = 0;
+
+private:
+    std::string callName;
+};
